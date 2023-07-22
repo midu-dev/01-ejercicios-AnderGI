30d3b13ec403c186dee4b05a4aa64b6d63b6c883
AnderGI <agibarguen@gmail.com>
2023-07-22 20:01:47 +0200

    Ejercicio 3

--- index.js
+++ index.js
@@ -1,50 +1,77 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
+const NO_DIRECTORY_OR_FILE_ERROR = "ENOENT";
+
 // Ejercicio 2
 async function writeFile(filePath, data, callback) {
-  const NO_DIRECTORY_OR_FILE_ERROR = "ENOENT";
   const exampleFile = "ejemplo.txt";
-  //ver si el path existe
+
   try {
     const pathStats = await fs.stat(filePath);
-    //Existe el path porque hemos obtenido el stat
-    //cuando la promesa resuelva puede ser un fichero o un directorio el path
-    //si es directorio añadimos fichero
+
     if (pathStats.isDirectory()) {
       filePath = path.join(filePath, exampleFile);
     }
 
-    //escribimos sobre ese fichero para ello esperamos a que esa promesa resuelva para seguir con le ejecucion de codigo
     await fs.writeFile(filePath, data, "utf8");
-    //llamamos al callback una vez se haya completado la escritura
-    //null porque no se han producido errores
-    callback(null);
+
+    return callback(null);
   } catch (err) {
-    //No existe el path por que no hemos obtenido el stat
     if (err.code === NO_DIRECTORY_OR_FILE_ERROR) {
-      //esperamos a que se resuelva la creacion del path
-      await fs.mkdir(filePath, { recursive: true });
+      try {
+        await fs.mkdir(filePath, { recursive: true });
 
-      //esperamos a que se obtenga el stat del path creado
-      const createdPathStat = await fs.stat(filePath);
+        const createdPathStat = await fs.stat(filePath);
 
-      //verificamos si es un direcotiro
-      if (createdPathStat.isDirectory()) {
-        filePath = path.join(filePath, exampleFile);
+        if (createdPathStat.isDirectory()) {
+          filePath = path.join(filePath, exampleFile);
+        }
+
+        await fs.writeFile(filePath, data, "utf8");
+
+        return callback(null);
+      } catch (err) {
+        console.log("Ha ocurrido un error");
       }
-
-      //escribimos sobre ese fichero para ello esperamos a que esa promesa resuelva para seguir con le ejecucion de codigo
-
-      await fs.writeFile(filePath, data, "utf8");
-
-      //llamamos al callback una vez se haya completado la escritura
-      callback(null);
     }
   }
 }
 
 // Ejercicio 3
-async function readFileAndCount(word, callback) {}
+async function readFileAndCount(word, callback) {
+  let wordCount = 0;
+  //lee el archivo de la linea de comandos
+  const file = process.argv[2];
+
+  if (!file) {
+    return callback(
+      new Error("No se ha especificado el path del archivo"),
+      wordCount
+    );
+  }
+
+  if (!word) {
+    return callback(
+      new Error("No se ha especificado la palabra a buscar"),
+      wordCount
+    );
+  }
+
+  //se le pasa como argumento el path
+  //ver si existe o no
+  try {
+    const readFile = await fs.readFile(file, "utf-8");
+    wordCount = readFile.split(" ").filter((w) => w.includes(word)).length;
+    return callback(null, wordCount);
+  } catch (err) {
+    if (err.code === NO_DIRECTORY_OR_FILE_ERROR) {
+      return callback(null, wordCount);
+    }
+
+    console.log("Ha ocurrido un error");
+    process.exit(1);
+  }
+}
 
 module.exports = {
   writeFile,
